1c7f71dd5b21964199073bde66f32ead90d57ed7
Andeskjerf <andreas.skjerven@pm.me>
2024-02-15 21:38:22 +0100

    refactoring

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-let loopId
-let hunger = 100
-let cleanliness = 100
-let playfulness = 100
-let hasBeenFed = false
-let hasBeenCleaned = false
-let hasBeenPlayed = false
-let isdead = false
-
+let loopId = null
+let values = [100, 100, 100]
+let recentValue = [false, false, false]
 
 function updateView() {
-    document.getElementById("app").innerHTML =/*HTML*/`
+  document.getElementById('app').innerHTML = /*HTML*/ `
         <div id="barContainer">
             <div class="bar" id="hungerBar"></div>
             <div class="bar" id="cleanlinessBar"></div>
@@ -22,75 +16,67 @@ function updateView() {
             <img class="bat noBatForYou" id="batDead" src="batAnimDead.gif">
         </div>
     <br>
-    <button class="feeder" onclick=feedBatt()>Eat</button>
-    <button class="cleaner" onclick=cleanBatt()>Clean</button>
-    <button class="player" onclick=playBatt()>Play</button>
+    <button class="feeder" onclick="incrementValue(0, 'hungerBar')">Eat</button>
+    <button class="cleaner" onclick="incrementValue(1, 'cleanlinessBar')">Clean</button>
+    <button class="player" onclick="incrementValue(2, 'playfulnessBar')">Play</button>
     `
 }
 
+function incrementValue(index, id) {
+  if (isDead()) return
+
+  values[index] += 28
+  recentValue[index] = true
+  if (values[index] > 100) values[index] = 100
+
+  updateGraph(id, index)
+
+  setTimeout(() => (recentValue[index] = false), 1000)
+}
+
+function setDead() {
+  document.getElementById('batDead').classList.toggle('noBatForYou')
+  document.getElementById('batBod').classList.toggle('noBatForYou')
+  clearInterval(loopId)
+  setTimeout(startDeathAnim, 1000)
+}
+
+function startDeathAnim() {
+  let image = document.createElement('img')
+  image.id = 'flightsim'
+  image.src = 'batAnimUndeadFlight.gif'
+  document.getElementById('batFaceNeut').classList.toggle('noBatForYou')
+  document.getElementById('batDead').classList.toggle('noBatForYou')
+  document.getElementById('batten').appendChild(image)
+  setTimeout(() => {
+    document.getElementById('flightsim').remove()
+  }, 1000)
+}
+
+function isDead() {
+  return values[0] <= 0 && values[1] <= 0 && values[2] <= 0
+}
+
+function tickValues() {
+  if (!recentValue[0]) values[0] -= 4
+  if (!recentValue[1]) values[1] -= 1
+  if (!recentValue[2]) values[2] -= 2
+}
+
+function updateGraphs() {
+  updateGraph('hungerBar', 0)
+  updateGraph('cleanlinessBar', 1)
+  updateGraph('playfulnessBar', 2)
+}
+
+function updateGraph(id, index) {
+  document.getElementById(id).style.height = values[index] + '%'
+}
+
 function loop() {
-    if (hunger <= 0 && cleanliness <= 0 && playfulness <= 0) {
-        document.getElementById('batDead').classList.toggle("noBatForYou")
-        clearInterval(loopId)
-        document.getElementById('batBod').classList.toggle('noBatForYou')
-        isdead = true
-        setTimeout(flyingBat, 1000)
-    }
-    if (!hasBeenFed) hunger -= 4
-    cleanliness -= 1
-    playfulness -= 2
-    document.getElementById('hungerBar').style.height = hunger + "%"
-    document.getElementById('cleanlinessBar').style.height = cleanliness + "%"
-    document.getElementById('playfulnessBar').style.height = playfulness + "%"
+  if (isDead()) setDead()
+  tickValues()
+  updateGraphs()
 }
 
-function flyingBat() {
-    let image = document.createElement("img")
-    image.id = "flightsim"
-    image.src="batAnimUndeadFlight.gif"
-    document.getElementById("batFaceNeut").classList.toggle("noBatForYou")
-    document.getElementById("batDead").classList.toggle("noBatForYou")
-    document.getElementById("batten").appendChild(image)
-    setTimeout(() => {
-        document.getElementById('flightsim').remove()
-    }, 1000);
-}
-
-loopId = setInterval(loop, 10)
-
-function feedBatt() {
-    if (isdead) return;
-    hunger += 28
-    if (hunger > 100)
-        hunger = 100
-    hasBeenFed = true
-    document.getElementById('hungerBar').style.height = hunger + "%"
-    setTimeout(setHasBeenFed, 1000)
-}
-function setHasBeenFed() {
-    hasBeenFed = false
-}
-function cleanBatt() {
-    if (isdead) return;
-    cleanliness += 28
-    if (cleanliness > 100)
-        cleanliness = 100
-    hasBeenCleaned = true
-    document.getElementById('cleanlinessBar').style.height = cleanliness + "%"
-    setTimeout(setHasBeenCleaned, 1000)
-}
-function setHasBeenCleaned() {
-    hasBeenCleaned = false
-}
-function playBatt() {
-    if (isdead) return;
-    playfulness += 28
-    if (playfulness > 100)
-        playfulness = 100
-    hasBeenPlayed = true
-    document.getElementById('playfulnessBar').style.height = playfulness + "%"
-    setTimeout(setHasBeenPlayed, 1000)
-}
-function setHasBeenPlayed() {
-    hasBeenPlayed = false
-}
+loopId = setInterval(loop, 500)
